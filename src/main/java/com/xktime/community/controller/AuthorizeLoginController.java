dc0a219905229a84beac828ba40cec32f6b78caf
xktime <luoliwen0913@foxmail.com>
2019-12-24 16:10:53 +0800

    修改Github权限验证方式

--- src/main/java/com/xktime/community/controller/AuthorizeLoginController.java
+++ src/main/java/com/xktime/community/controller/AuthorizeLoginController.java
@@ -1,19 +1,19 @@
 package com.xktime.community.controller;
 
-import com.xktime.community.model.dto.GithubUserDTO;
 import com.xktime.community.model.entity.User;
 import com.xktime.community.service.GithubLoginService;
 import com.xktime.community.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
+@RequestMapping("callback")
 public class AuthorizeLoginController {
 
     @Autowired
@@ -21,14 +21,16 @@ public class AuthorizeLoginController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code,
+    @GetMapping("/github")
+    public String github(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
                            HttpServletResponse response) {
         User user = githubService.getUser(code, state);
         if (user != null) {
             userService.saveUser(user);
-            response.addCookie(new Cookie("token", user.getToken()));
+            Cookie cookie = new Cookie("token", user.getToken());
+            cookie.setPath("/");
+            response.addCookie(cookie);
             return "redirect:/";
         } else {
             return "redirect:/";
