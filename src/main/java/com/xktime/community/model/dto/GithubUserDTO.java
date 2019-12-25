84d476a22631ac05d08f87653d2dc73fedbf553b
xktime <luoliwen0913@foxmail.com>
2019-12-25 21:13:21 +0800

    增加lombok支持

--- src/main/java/com/xktime/community/model/dto/GithubUserDTO.java
+++ src/main/java/com/xktime/community/model/dto/GithubUserDTO.java
@@ -1,50 +1,11 @@
 package com.xktime.community.model.dto;
 
+import lombok.Data;
+
+@Data
 public class GithubUserDTO {
     private String name;
     private String id;
     private String bio;
     private String login;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    @Override
-    public String toString() {
-        return "GithubUser{" +
-                "name='" + name + '\'' +
-                ", id='" + id + '\'' +
-                ", bio='" + bio + '\'' +
-                ", login='" + login + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/xktime/community/model/entity/User.java
+++ src/main/java/com/xktime/community/model/entity/User.java
@@ -1,7 +1,10 @@
 package com.xktime.community.model.entity;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class User {
     private int id;
     private String name;
@@ -10,60 +13,4 @@ public class User {
     private String login;
     private String token;
     private Date loginTime;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAccount_id() {
-        return account_id;
-    }
-
-    public void setAccount_id(String account_id) {
-        this.account_id = account_id;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Date getLoginTime() {
-        return loginTime;
-    }
-
-    public void setLoginTime(Date loginTime) {
-        this.loginTime = loginTime;
-    }
 }
