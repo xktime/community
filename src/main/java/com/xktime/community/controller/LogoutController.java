5c719b52a5dacf1488616ed9de8302b6b5b67a08
xktime <luoliwen0913@foxmail.com>
2019-12-25 14:56:34 +0800

    添加注销功能

---
+++ src/main/java/com/xktime/community/controller/LogoutController.java
@@ -0,0 +1,21 @@
+package com.xktime.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class LogoutController {
+    @GetMapping("logout")
+    public String logout(HttpServletRequest request,
+                         HttpServletResponse response) {
+        request.getSession().removeAttribute("user");
+        Cookie cookie = new Cookie("token", null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "redirect:/";
+    }
+}
