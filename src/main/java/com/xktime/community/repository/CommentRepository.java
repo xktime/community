1f3ab1e6cf6e731b2af5212f9dc3ebf4675747b1
xktime <luoliwen0913@foxmail.com>
2020-03-03 17:04:03 +0800

    修改MyBatis映射方式

--- src/main/java/com/xktime/community/repository/CommentRepository.java
+++ src/main/java/com/xktime/community/repository/CommentRepository.java
@@ -14,31 +14,21 @@ import java.util.List;
 @Repository
 public interface CommentRepository {
 
-    @Insert("INSERT INTO comment(article_id,author_account_id,post_time,content) " +
-            "VALUES (#{articleId},#{authorAccountId},#{postTime},#{content}) ")
     void saveComment(Comment comment);
 
-    @Select("SELECT * FROM comment WHERE article_id = #{articleId}")
     List<Comment> findByArticleId(@Param("articleId") int articleId);
 
-    @Select("SELECT * FROM comment WHERE author_account_id = #{accountId}")
     List<Comment> findByAccountId(@Param("accountId") String accountId);
 
-    @Select("SELECT * FROM comment WHERE article_id = #{articleId} " +
-            "LIMIT #{pageShowNum} OFFSET #{pageStartIndex}")
     List<Comment> findByOffsetAndArticleId(@Param("pageStartIndex") int pageStartIndex,
                                            @Param("pageShowNum") int pageShowNum,
                                            @Param("articleId") int articleId);
 
-    @Select("SELECT * FROM comment WHERE author_account_id = #{accountId} " +
-            "LIMIT #{pageShowNum} OFFSET #{pageStartIndex}")
     List<Comment> findByOffsetAndAccountId(@Param("pageStartIndex") int pageStartIndex,
                                            @Param("pageShowNum") int pageShowNum,
                                            @Param("accountId") String accountId);
 
-    @Select("SELECT COUNT(*) FROM comment WHERE article_id = #{articleId}")
     int getCommentCountByArticleId(@Param("articleId") int articleId);
 
-    @Select("SELECT COUNT(*) FROM comment WHERE author_account_id = #{accountId}")
     int getCommentCountByAccountId(@Param("accountId") String accountId);
 }
--- src/main/java/com/xktime/community/repository/UserRepository.java
+++ src/main/java/com/xktime/community/repository/UserRepository.java
@@ -7,18 +7,12 @@ import org.springframework.stereotype.Repository;
 @Mapper
 @Repository
 public interface UserRepository {
-    @Insert("INSERT INTO user(name,account_id,bio,login,token,login_time,avatar_url) " +
-            "VALUES (#{name},#{accountId},#{bio},#{login},#{token},#{loginTime},#{avatarUrl})")
     void saveUser(User user);
 
-    @Update("UPDATE user SET token = #{token}, login_time = #{loginTime}, bio = #{bio}, name = #{name}, avatar_url = #{avatarUrl}" +
-            "WHERE account_id = #{accountId}")
     void updateUser(User user);
 
-    @Select("SELECT * FROM user WHERE token = #{token}")
     User findByToken(@Param("token") String token);
 
-    @Select("SELECT * FROM user WHERE account_id = #{accountId}")
     User findByAccountId(@Param("accountId") String accountId);
 
 }
